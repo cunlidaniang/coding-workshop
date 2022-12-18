66ef20d507cf2afa56dc26357372b29a3e1c7b56
cunlidaniang <770871922@qq.com>
2022-12-18 21:18:30 +0800

    commit

--- c++ workshop/project 5/include/Matrix.hpp
+++ c++ workshop/project 5/include/Matrix.hpp
@@ -8,16 +8,21 @@ class Matrix{
     private:
     size_t row;           //row number
     size_t col;           //column number
-    shared_ptr<T *> entry;//store all entries in wich entry[(i - 1)* step + (j - 1)] is entry in (i, j)
+    shared_ptr<T *> entry;//store all entries in wich (*entry)[(i - 1)* step + (j - 1)] is entry in (i, j)
     size_t step;          //step length
 
     public:
+    //not allowed.
     Matrix() = delete;
-
-    Matrix(size_t row, size_t col):row(row), col(col), step(col){
+    //set entry to be shared_ptr
+    Matrix(size_t row, size_t col, size_t step = col):row(row), col(col), step(step){
         entry = make_shared<T *>( (T *) malloc(col * row * sizeof(T) ) );
     }
-
+    //delete function. need special care for shared_ptr
+    ~Matrix(){
+        entry = nullptr;
+    }
+    //return true IFF all the entry value are equal
     bool operator == (const Matrix & o){
         if(*this == o){
             return true;
@@ -34,7 +39,7 @@ class Matrix{
         }
         return true;
     }
-
+    //return the matrix equal to the sum of two matrix; throw error IFF size not match
     Matrix operator + (const Matrix & o){
         Matrix ans(row, col);
         if(row != o.row || col != o.col){
@@ -47,7 +52,7 @@ class Matrix{
         }
         return ans;
     }
-
+    //return the matrix equal to the sub of two matrix; throw error IFF size not match
     Matrix operator - (const Matrix & o){
         Matrix ans(row, col);
         if(row != o.row || col != o.col){
@@ -60,7 +65,7 @@ class Matrix{
         }
         return ans;
     }
-
+    //return the matrix equal to the mul of two matrix; throw error IFF size not match
     Matrix operator * (const Matrix & o){
         Matrix ans(row, o.col);
         if(col != o.row){
@@ -80,7 +85,21 @@ class Matrix{
         }
         return ans;
     }
-
+    //get the part of matrix by using shared_ptr
+    Matrix getROI(size_t prow, size_t pcol, size_t sizeRow, size_t sizeCol){
+        Matrix ans(sizeRow, sizeCol, step);
+        ans.entry = make_shared<T *> ((*entry)+ prow * step + pcol);
+        return ans;
+    }
+    //get the value in entry[r][c]
+    T & get(size_t r, size_t c){
+        return (*entry)[(r - 1) * step + (c - 1)];
+    }
+    //set the value in entry[r][c]
+    void set(size_t r, size_t c, T v){
+        (*entry)[(r - 1) * step + (c - 1)] = v;
+    }
+    //print this matrix on bash
     void print(){
         for(size_t i = 0;i< row;i++){
             for(size_t j = 0;j< col;j++){
@@ -88,24 +107,6 @@ class Matrix{
             }
             cout<< endl;
         }
-    }
-
-    Matrix getROI(size_t prow, size_t pcol, size_t sizeRow, size_t sizeCol){
-        Matrix ans(sizeRow, sizeCol);
-        ans.entry = make_shared<T *> ((*entry)+ prow * step + pcol);
-        return ans;
-    }
-
-    T & get(size_t r, size_t c){
-        return (*entry)[(r - 1) * step + (c - 1)];
-    }
-
-    void set(size_t r, size_t c, T v){
-        (*entry)[(r - 1) * step + (c - 1)] = v;
-    }
-
-    ~Matrix(){
-        entry = nullptr;
     }
 };
 
